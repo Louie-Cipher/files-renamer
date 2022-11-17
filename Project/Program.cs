b5710ebec99c600dfee4d387c9b4140b7dc8cb91
Louie-Cipher <louie.spena@outlook.com>
2022-11-16 21:19:16 -0300

    move source to Project dir

---
+++ Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FilesRenamer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) RunMode.Guided();
+            else RunMode.Advanced(args);
+        }
+    }
+}
